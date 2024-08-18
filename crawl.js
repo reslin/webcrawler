29012128628519f847127fb3b92f74770facaea2
reslin <uwe.ploss@gmail.com>
2024-08-18 23:26:29 +0200

    getURLsFromHTML fun and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,33 @@
+const { JSDOM } = require('jsdom')
+
+function getURLsFromHTML(htmlBody, baseURL) {
+    //console.log(htmlBody)
+    //console.log(baseURL)
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+    
+    for (const linkElement of linkElements) {
+        //console.log(linkElement.href)
+        if (linkElement.href.slice(0, 1) === '/') {
+            try {
+                const urlObj = new URL(`${baseURL}${linkElement.href}`)
+                urls.push(urlObj.href)
+            } catch (err){
+                console.log(`error with: ${err.message}`)
+            }
+        } else {
+            try {
+                const urlObj = new URL(linkElement.href)
+                urls.push(urlObj.href)
+            } catch (err){
+                console.log(`error with: ${err.message}`)
+            }
+        }
+    }
+    return urls
+}
+
 function normalizeURL(urlString) {
     const urlObj = new URL(urlString)
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`
@@ -8,5 +38,6 @@ function normalizeURL(urlString) {
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeURL } = require('./crawl.js')
+const { normalizeURL, getURLsFromHTML } = require('./crawl.js')
 const { test, expect } = require('@jest/globals')
 
 test('normalizeURL strip protocol', () => {
@@ -8,6 +8,7 @@ test('normalizeURL strip protocol', () => {
     expect(actual).toEqual(expected)
 })
 
+
 test('normalizeURL strip trailing slash', () => {
     const input = 'http://bla.blabla.com/path/'
     const actual = normalizeURL(input)
@@ -15,9 +16,72 @@ test('normalizeURL strip trailing slash', () => {
     expect(actual).toEqual(expected)
 })
 
+
 test('normalizeURL UPPERCASE domain', () => {
     const input = 'http://BLA.blAbla.com/path/'
     const actual = normalizeURL(input)
     const expected = 'bla.blabla.com/path'
+    expect(actual).toEqual(expected)
+})
+
+
+test('getURLsFromHTML absolute', () => {
+    const inputHTMLBody = `
+<html>
+    <body>
+        <a href="https://abc.def.gh/path/">iuiuiu</a>
+    </body
+</html>
+`
+    const inputBaseURL = 'https://abc.def.gh/path/'
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = ['https://abc.def.gh/path/']
+    expect(actual).toEqual(expected)
+})
+
+
+test('getURLsFromHTML relative', () => {
+    const inputHTMLBody = `
+<html>
+    <body>
+        <a href="/path/">jhgjhg</a>
+    </body
+</html>
+`
+const inputBaseURL = 'https://abc.def.gh'
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = ['https://abc.def.gh/path/']
+    expect(actual).toEqual(expected)
+})
+
+
+test('getURLsFromHTML invalid', () => {
+    const inputHTMLBody = `
+<html>
+    <body>
+        <a href="invalid">jhgjhg</a>
+    </body
+</html>
+`
+const inputBaseURL = 'https://abc.def.gh'
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = []
+    expect(actual).toEqual(expected)
+})
+
+
+test('getURLsFromHTML abs + relative', () => {
+    const inputHTMLBody = `
+<html>
+    <body>
+    <a href="https://abc.def.gh/path1/">iuiuiu</a>
+    <a href="/path2/">jhgjhg</a>
+    </body
+</html>
+`
+
+const inputBaseURL = 'https://abc.def.gh'
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = ['https://abc.def.gh/path1/', 'https://abc.def.gh/path2/']
     expect(actual).toEqual(expected)
 })
